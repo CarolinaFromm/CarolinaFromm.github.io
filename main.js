b07f06fcaac9b741c5974dc60a7c69d41b0cc311
CarroFromm <carolina.fromm@yh.nackademin.se>
2025-03-21 13:12:26 +0100

    Lite nytt JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('Hello world!') 
+//alert('Hello world!') 
+
+// String, Numbers, Boolean
+
+// concatenation
+
+const name = 'Carolina';
+const age = 30;
+
+console.log('My name is' + name + ' and I am')
